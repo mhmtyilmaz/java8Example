2642a85adfa247086da87f99be345421dc2cf0aa
mhmtyilmaz <mylmz89@gmail.com>
2017-07-22 14:33:26 +0300

    example of lambda and method reference

--- src/lambdafunctionalinterface/methodreferences/BinaryOperationFunction.java
+++ src/lambdafunctionalinterface/methodreferences/BinaryOperationFunction.java
@@ -1,7 +1,21 @@
 package lambdafunctionalinterface.methodreferences;
 
+import java.util.function.BinaryOperator;
+
 /**
  * Created by MEHMET on 22.7.2017.
  */
-public class BinaryOperationFunction {
+public class BinaryOperationFunction  {
+
+    public static void main(String[] args){
+
+        BinaryOperator<Integer> sum = (i1,i2) -> i1 + i2;
+
+        //make clean code
+        BinaryOperator<Integer> sumInt = (i1,i2) -> Integer.sum(i1, i2);
+
+        //with method reference
+        BinaryOperator<Integer> sumMR = Integer::sum;
+        BinaryOperator<Integer> max = Integer::max;
+    }
 }
--- src/lambdafunctionalinterface/methodreferences/Person.java
+++ src/lambdafunctionalinterface/methodreferences/Person.java
@@ -4,4 +4,13 @@ package lambdafunctionalinterface.methodreferences;
  * Created by MEHMET on 22.7.2017.
  */
 public class Person {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
